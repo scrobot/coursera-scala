f72c43da13388a86291506d41ca89bbebe78d935
Alex Scrobot <scrobot91@gmail.com>
2017-07-02 14:25:56 +0700

    scala education init

---
+++ example/src/main/scala/example/Main.scala
@@ -0,0 +1,10 @@
+package example
+
+/**
+  * Created by aleksejskrobot on 02.07.17.
+  */
+object Main extends App {
+
+  println(Lists.max(List(0,1,2,0,4,12,9)))
+
+}
---
+++ test/src/main/scala/Example.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by aleksejskrobot on 01.07.17.
+  */
+object Example extends App {
+
+  println("Hello world")
+
+}
